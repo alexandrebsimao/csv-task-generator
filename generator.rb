9f665a7d3c7f7a75297c646a418ce830c1c4e8db
Alexandre A. Berns Simão <alexandreabs92@gmail.com>
2021-06-16 22:29:09 -0300

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -2,7 +2,7 @@ require 'active_support/all'
 
 str = "ID;Atividade;Situacao;Inicio;Fim;Duracao;Recurso\n"
 
-situations = { 'pending' => 1440, 'documentation' => 480, 'development' => 1440, 'test' => 1440, 'reworking' => 480, 'finished' => 60}
+situations = { 'pending' => 1440, 'documentation' => 480, 'development' => 2400, 'test' => 2400, 'reworking' => 480, 'finished' => 60}
 resources = [ 'Bob', 'Jonh', 'Mary', 'Ashley', 'Marie', 'Joana', 'Luke', 'Rob' ]
 
 start_time = '2012-05-06 08:00:00'.to_datetime
@@ -21,7 +21,9 @@ resource_id = 0
   resource = resources[resource_id]
 
   situations.each do |sit, time|
-    duration = rand((time - (time * 0.10))..(time + (time * 0.10))).to_i.minutes
+    next if sit == 'reworking' && rand(0..1)
+
+    duration = rand((time - (time * 0.20))..(time + (time * 0.20))).to_i.minutes
     start_time = end_time != nil ? end_time : start_time
     end_time = start_time.to_datetime + duration
 
@@ -31,22 +33,17 @@ resource_id = 0
 
     str += "#{count};#{name};#{situation};#{start_time};#{end_time};#{duration};#{resource}\n"
   end
-  
-  puts resource_id
-  puts max_date
 
   if resource_id == resources.size
     resource_id = 0
     start_time = max_date
     end_time = max_date
   else
-    # start_time = '2020-05-06 08:00:00'.to_datetime
     max_date = end_time if max_date == nil || max_date < end_time
     end_time = nil
   end
 
   resource_id += 1
-
 end
 
 puts str
